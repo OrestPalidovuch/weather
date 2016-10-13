726d3a3ebfe44e2c0a31f614aa838e66fed4f69e
orest palidovych <orest989@gmail.com>
2016-10-13 14:02:00 +0300

    making only letters keypres in input field

--- index.js
+++ index.js
@@ -23,3 +23,14 @@ $('#myField').on('input', function() {
 $('#clear').on('click', function(){
   $( ".weather" ).empty();
 });
+$('input').on('keypress',function lettersOnly(evt) {
+       evt = (evt) ? evt : event;
+       var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+          ((evt.which) ? evt.which : 0));
+       if (charCode > 31 && (charCode < 65 || charCode > 90) &&
+          (charCode < 97 || charCode > 122)) {
+          alert("Enter letters only.");
+          return false;
+       }
+       return true;
+     });
