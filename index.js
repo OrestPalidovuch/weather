541c2926ef9b6727971f6046a2ac4af5a9201420
orest palidovych <orest989@gmail.com>
2016-10-13 14:12:04 +0300

    correct alert

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ $('input').on('keypress',function lettersOnly(evt) {
           ((evt.which) ? evt.which : 0));
        if (charCode > 31 && (charCode < 65 || charCode > 90) &&
           (charCode < 97 || charCode > 122)) {
-          alert("Enter letters only.");
+          alert("Enter english letters only.");
           return false;
        }
        return true;
